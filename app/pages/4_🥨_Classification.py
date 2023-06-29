744896868f5654eb93086131387f673f3d28c279
ge24wej <kevo.yuan@tum.de>
2023-06-29 23:16:19 +0200

    clf

--- app/pages/4_🥨_Classification.py
+++ app/pages/4_🥨_Classification.py
@@ -1,12 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 from streamlit_toggle import st_toggle_switch
-
-
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neural_network import MLPClassifier
 from streamlit_extras.switch_page_button import switch_page
 from streamlit_extras.add_vertical_space import add_vertical_space
 
@@ -15,8 +9,8 @@ st.set_page_config(
     page_icon="🎯",
     # layout="wide"
 )
-classifier = option_menu('Classification Model', ['KNN', 'SVC', 'Random Forest', 'MLP'],
-                         icons=['-', '-', "-", '-'],
+classifier = option_menu('Classification Model', ['KNN', 'SVC', 'Random Forest', 'MLP', 'VAE'],
+                         icons=['-', '-', '-', '-', '-'],
                          menu_icon="bi bi-diagram-3-fill", default_index=0, orientation="horizontal",
                          styles={
     "container": {
@@ -29,10 +23,10 @@ classifier = option_menu('Classification Model', ['KNN', 'SVC', 'Random Forest',
         "list-style-type": "none",
         "margin": "0"
     },
-    "menu-title":{
+    "menu-title": {
         "color": "#333333",
         "font-weight": "bold"
-        
+
     },
     "nav-link": {
         "font-size": "15px",
@@ -68,23 +62,19 @@ if classifier == 'KNN':
     with col1:
         n_neighbors = st.slider(
             'n_neighbors', min_value=2, max_value=10, value=5)
-
-    with col2:
-        weights = st.selectbox('weights', ('uniform', 'distance'))
-    col3, col4 = st.columns(2)
-    with col3:
         algorithm = st.selectbox(
             'algorithm', ('auto', 'ball_tree', 'kd_tree', 'brute'))
 
-    with col4:
+    with col2:
+        weights = st.selectbox('weights', ('uniform', 'distance'))
         metric = st.selectbox('metric', ('l1', 'l2'))
 
-    knn_clf = KNeighborsClassifier(
-        n_neighbors=n_neighbors,
-        weights=weights,
-        algorithm=algorithm,
-        metric=metric
-    )
+    knn_params = {
+        "n_neighbors": n_neighbors,
+        "algorithm": algorithm,
+        "weights": weights,
+        "metric": metric,
+    }
 
 
 elif classifier == "SVC":
@@ -92,19 +82,19 @@ elif classifier == "SVC":
     col1, col2 = st.columns(2)
     with col1:
         C = st.slider('C', min_value=0.0, max_value=1.0, value=1.0)
+        gamma = st.selectbox('gamma', ('scale', 'auto'))
+
     with col2:
         kernel = st.selectbox(
             'kernel', ('linear', 'rbf', 'sigmoid', 'precomputed'))
 
     col1, col2 = st.columns(2)
-    with col1:
-        gamma = st.selectbox('gamma', ('scale', 'auto'))
 
-    svc_clf = SVC(
-        C=C,
-        kernel=kernel,
-        gamma=gamma
-    )
+    svc_params = {
+        'C': C,
+        'kernel': kernel,
+        'gamma': gamma,
+    }
 
 elif classifier == "Random Forest":
     st.header("Random Forest")
@@ -112,22 +102,18 @@ elif classifier == "Random Forest":
     with col1:
         n_estimators = st.slider(
             'n_estimators', min_value=1, max_value=200, value=100)
-    with col2:
-        criterion = st.selectbox('criterion', ('gini', 'entropy', 'log_loss'))
-
-    col1, col2 = st.columns(2)
-    with col1:
         max_depth = st.slider('max_depth', min_value=1,
                               max_value=100, value=10)
     with col2:
+        criterion = st.selectbox('criterion', ('gini', 'entropy', 'log_loss'))
         max_features = st.selectbox('max_features', ('sqrt', 'log2', None))
 
-    rf_clf = RandomForestClassifier(
-        n_estimators=n_estimators,
-        criterion=criterion,
-        max_depth=max_depth,
-        max_features=max_features
-    )
+    rf_params = {
+        'n_estimators': n_estimators,
+        'criterion': criterion,
+        'max_depth': max_depth,
+        'max_features': max_features
+    }
 
 elif classifier == "MLP":
     st.header("MLP")
@@ -136,47 +122,66 @@ elif classifier == "MLP":
     with col1:
         hidden_layer_sizes = st.slider(
             'hidden_layer_sizes', min_value=1, max_value=100, value=50)
+        solver = st.selectbox('solver', ('lbfgs', 'sgd', 'adam'))
+        learning_rate = st.selectbox(
+            'learning_rate', ('constant', 'invscaling', 'adaptive'))
+
     with col2:
         activation = st.selectbox(
             'activation', ('identity', 'logistic', 'tanh', 'relu'))
-    col1, col2 = st.columns(2)
-
-    with col1:
-        solver = st.selectbox('solver', ('lbfgs', 'sgd', 'adam'))
-    with col2:
         alpha = st.number_input('alpha', value=1e-4, format="%.4f")
-    col1, col2 = st.columns(2)
-
-    with col1:
-        learning_rate = st.selectbox(
-            'learning_rate', ('constant', 'invscaling', 'adaptive'))
-    with col2:
         learning_rate_init = st.slider(
             'learning_rate_init', min_value=0.001, max_value=0.1, value=0.001)
 
-    mlp_clf = MLPClassifier(
-        hidden_layer_sizes=(hidden_layer_sizes,),
-        activation=activation,
-        solver=solver,
-        alpha=alpha,
-        learning_rate=learning_rate,
-        learning_rate_init=learning_rate_init
-    )
+    mlp_params = {
+        'hidden_layer_sizes': hidden_layer_sizes,
+        'activation': activation,
+        'solver': solver,
+        'alpha': alpha,
+        'learning_rate': learning_rate,
+        'learning_rate_init': learning_rate_init
+    }
+
+elif classifier == "VAE":
+    st.header("VAE")
+    col1, col2 = st.columns(2)
+    with col1:
+        hidden_layers = st.slider(
+            'hidden_layers', min_value=1, max_value=10, value=5)
+        hidden_layer_sizes = st.slider(
+            'hidden_layer_sizes', min_value=1, max_value=100, value=50)
+        solver = st.selectbox('solver', ('lbfgs', 'sgd', 'adam'))
+        learning_rate = st.selectbox(
+            'learning_rate', ('constant', 'invscaling', 'adaptive'))
+
+    with col2:
+        activation = st.selectbox(
+            'activation', ('identity', 'logistic', 'tanh', 'relu'))
+        alpha = st.number_input('alpha', value=1e-4, format="%.4f")
+        learning_rate_init = st.slider(
+            'learning_rate_init', min_value=0.001, max_value=0.1, value=0.001)
+
+    vae_params = {
+        'hidden_layers': hidden_layers,
+        'hidden_layer_sizes': hidden_layer_sizes,
+        'activation': activation,
+        'solver': solver,
+        'alpha': alpha,
+        'learning_rate': learning_rate,
+        'learning_rate_init': learning_rate_init
+    }
 
 
-# Create three columns
-
-classifiers = {
-    'KNN': knn_clf if 'knn_clf' in locals() else None,
-    'SVC': svc_clf if 'svc_clf' in locals() else None,
-    'Random Forest': rf_clf if 'rf_clf' in locals() else None,
-    'MLP': mlp_clf if 'mlp_clf' in locals() else None,
-}
-
-selected_classifier = classifiers[classifier]
-
-# dict
-selected_classifier_dict = vars(selected_classifier)
+if classifier == 'KNN':
+    selected_classifier_dict = knn_params
+if classifier == 'SVC':
+    selected_classifier_dict = svc_params
+if classifier == 'Random Forest':
+    selected_classifier_dict = rf_params
+if classifier == 'MLP':
+    selected_classifier_dict = mlp_params
+if classifier == 'VAE':
+    selected_classifier_dict = vae_params
 
 
 with st.sidebar:
