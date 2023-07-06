696fccf1a394c7759d19b3a9c756bfad22028070
ge24wej <kevo.yuan@tum.de>
2023-07-06 13:44:21 +0200

    up

--- app/app.py
+++ app/app.py
@@ -1,11 +1,10 @@
-import time
 import streamlit as st
-import randfacts
-from streamlit_extras.add_vertical_space import add_vertical_space
-from PIL import Image
 import os
+import shutil
+from streamlit_toggle import st_toggle_switch
+from user_util import get_directory_size
 from streamlit_extras.switch_page_button import switch_page
-from detection import get_image_masks
+import time
 
 st.set_page_config(
     page_title="AMI05",
@@ -13,168 +12,107 @@ st.set_page_config(
     # layout="wide"
 )
 
-# parameters_segmentation_stack = {
-#     'CHECKPOINT_PATH': os.path.join("app", "sam_weights", "sam_vit_b_01ec64.pth"),
-#     'MODEL_TYPE': "vit_b",
-#     'DEVICE': torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-# }
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden; }
+        footer {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+# function to create a new folder
 
 
-def upload_images(image_folder):
-    uploaded_files = st.sidebar.file_uploader(
-        "Choose image files", type=["png", "jpg", "jpeg"], accept_multiple_files=True)
-
-    images = []
-    image_paths = []  # This will hold the paths of the saved images
-
-    if uploaded_files:
-        for uploaded_file in uploaded_files:
-            # Convert the file to an image
-            image = Image.open(uploaded_file)
-            # Save the image to the image folder
-            image_path = os.path.join(image_folder, uploaded_file.name)
-            image.save(image_path)
-            images.append(image)
-
-            # Append the path of the saved image to the list
-            image_paths.append(image_path)
-
-    return images, image_paths 
+def create_folder(username):
+    folder_path = f'./user_folders/{username}'
+    os.makedirs(folder_path, exist_ok=True)
+    # This line opens the file for writing, overwriting the file if it exists
+    with open('./username.txt', 'w') as file:
+        file.write(username)
 
 
-def generate_cork_board(fun_info):
-    css = f"""
-    <style>
-    div#frame {{
-        display: flex; 
-        justify-content: center;  
-        align-items: center;  
-        background-color: #F0F0F0;
-        width: 100%;
-        height: 250px;
-        # padding-top: 35px;
-        # padding-left: 35px;
-        box-shadow: 0 2px 5px rgba(0,0,0,.15);
-    }}
+def main():
 
-    .note {{
-        width: 500px;
-        height: 150px;
-        box-shadow: 0 3px 6px rgba(0,0,0,.25);
-        float: left;
-        margin: 0px;
-        border: 1px solid rgba(0,0,0,.15);
-        background-color: #FFFFFF;
-        border-radius: 10px;
-    }}
+    st.header("User Account Management")
 
-    .text_board {{
-        margin: 10px;
-        font-family: 'Helvetica Neue', sans-serif;
-    }}
-    </style>
-    """
+    # Input to change username in session state
+    username = st.text_input(
+        "User Name", key='username')
 
-    cork_board = f"""
-    <div id='frame'>
-        <div class="note sticky1">
-            <div class='text_board'>Do you know: {fun_info}</div>
-        </div>
-    </div>
-    """
+    menu = ["Login", "SignUp"]
+    choice = st.radio('-', menu)
 
-    return css + cork_board
+    if choice == "Home":
+        st.write("Home")
 
-
-##########################################################
-
-
-image_folder = "image/"
-uploaded_image, image_paths = upload_images(image_folder)
-
-
-SAVE_ROOT_PATH = "data"
-# st.write(image_paths)
-# st.write(len(image_paths))
-
-
-if not uploaded_image or not image_paths:
-    st.warning("Please upload images")
-else:
-    pass
-    # num_images = len(uploaded_image)
-    # max_columns = 4
-    # num_rows = (num_images + max_columns - 1) // max_columns
-
-    # for i in range(num_rows):
-    #     columns = st.columns(max_columns)
-    #     for j in range(max_columns):
-    #         image_index = i * max_columns + j
-    #         if image_index < num_images:
-    #             columns[j].image(uploaded_image[image_index])
-    #             columns[j].write(f"images{j}")
+    elif choice == "Login":
+        if st.button("Login"):
+            if os.path.isdir(f'./user_folders/{username}'):
+                with open('./username.txt', 'w') as file:
+                    file.write(username)
                 
-if uploaded_image:
-    # Divide the width equally among the number of images
-    st.write("preview of the images")
-    
-    columns = st.columns(4)  # Create 4 columns
-    for i, image_path in enumerate(image_paths):
-        image = Image.open(image_path)
-        with columns[i % 4]:  # Select the column by index
-            st.image(image, caption=os.path.basename(image_path), use_column_width=True)
-    if st.sidebar.button("🧀 Segmentaiton"):
-        st.empty()
-        fun_info = randfacts.get_fact()
-        st.markdown(generate_cork_board(fun_info), unsafe_allow_html=True)
-        add_vertical_space(2)
-        with st.spinner('Wait for it...'):
-
-            # macOS style gradient
-            gradient = "linear-gradient(to right, #4cd964, #5ac8fa, #007aff, #34aadc, #5856d6, #ff2d55)"
-
-            # # Create a placeholder for the progress bar
-            # progress_placeholder = st.empty()
-
-            total_images = len(image_paths)
-            progress_placeholders = st.empty()
-
-            for i, image_path in enumerate(image_paths):
+                st.success(f"Logged in as {username}", icon="✅")
+                time.sleep(0.5)
+                switch_page("Segmentation")
                 
-                dir = os.path.splitext(os.path.basename(image_path))[0]
-                print('dir: ', dir)
-                print('image_path: ', image_path)
+                
+            else:
+                st.warning("Invalid Username")
 
-                os.makedirs(os.path.join(SAVE_ROOT_PATH, dir), exist_ok=True)
-                st.write(os.path.basename(image_path))
-
-                get_image_masks(
-                    i,
-                    dir, 
-                    image_path, 
-                    save_as_pkl=True, 
-                    save_annotated=True, 
-                    return_elapsed_time=True, 
-                    return_annotated=True, 
-                    )
-
-                # comparison.append(comparison_image)
-                # masks.append(mask)
-                # durations.append(duration)
-                add_vertical_space(1)
-
-                col1, col2 = st.columns(2)
-                col1.image(image_path,caption='Orignal image')
-                col2.image(f"{SAVE_ROOT_PATH}/{dir}/bbox.png",caption='Segmented image')
-                time.sleep(0.1)
-            time.sleep(1)
-
-            st.success('Done!', icon="✅")
-            # st.experimental_rerun()
-
- 
+    elif choice == "SignUp":
+        if st.button("Create Account"):
+            if not os.path.isdir(f'./user_folders/{username}'):
+                create_folder(username)
+                st.success(f"Account created for {username}")
+            else:
+                st.warning("User already exists")
 
 
-if st.sidebar.button("🤙🏻 Summit"):
+def delete_user_folder(username):
+    folder_path = f'./user_folders/{username}'
+    if os.path.isdir(folder_path):
+        shutil.rmtree(folder_path)
+        st.success(f"User {username} deleted successfully.")
+    else:
+        st.warning("User does not exist.")
 
-    switch_page("Labeling")
+
+
+
+
+if __name__ == '__main__':
+    main()
+    with st.sidebar:
+        on = st_toggle_switch(
+            label="Account Management",
+            key="switch_1",
+            default_value=False,
+            label_after=True,
+            inactive_color="#D3D3D3",
+            active_color="#11567f",
+            track_color="#29B5E8",
+        )
+
+        if on:
+            # List all user folders
+            user_folders = [folder for folder in os.listdir(
+                './user_folders') if folder != '.DS_Store']
+
+            # Create a Streamlit selectbox for the user folders
+            selected_user = st.selectbox("Select a user", user_folders)
+            # Calculate and display the size of the selected user's folder
+            folder_size = get_directory_size(f'./user_folders/{selected_user}')
+            st.write(
+                f"The {selected_user} folder is {folder_size / (1024 * 1024):.2f} MB")
+
+            # Create a Streamlit button
+            toggle_button = st.checkbox("Delete selected user")
+
+            if toggle_button:
+                st.warning(
+                    f"Warning: This process will delete all files in the {selected_user} folder!")
+                button_clicked = st.button("Confirm")
+
+                # Check if the button is clicked
+                if button_clicked:
+                    delete_user_folder(selected_user)
